5e73f1734c0582ed3a14ac497e1a7babc44d6358
alexzg <azlex0@gmail.com>
2019-01-09 12:19:21 +0100

    Create Error.js

---
+++ udemy-assignment-projects/src/assignment3/containers/pages/Error/Error.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Error extends Component {
+	render() {
+		return(
+			<div>
+				<h2>Page not Found (404)</h2>
+			</div>
+		)
+	}
+}
+
+export default Error;
