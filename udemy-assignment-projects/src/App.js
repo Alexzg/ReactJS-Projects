a0d9486b60c2ecf42324a430cf1bf2303200d7b1
alexzg <azlex0@gmail.com>
2019-02-09 10:02:43 +0100

    Update App.js

--- udemy-assignment-projects/src/App.js
+++ udemy-assignment-projects/src/App.js
@@ -4,14 +4,22 @@ import './App.css';
 
 //import Assignment from './assignment1/App';
 //import Assignment from './assignment2/App';
-import Assignment from './assignment3/App';
+//import Assignment from './assignment3/App';
+
+import Assignment from './assignment4/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './assignment4/store/reducer';
+const store = createStore(reducer);
 
 class App extends Component {
   render() {
     return (
+	<Provider store={store}>
       <div className="App">
-	  <Assignment />
+		<Assignment />
       </div>
+	</Provider>
     );
   }
 }
